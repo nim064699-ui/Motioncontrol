2e0672f97566be5b7127a3000be767a52de39be6
nim064699-ui <nim064699@gmail.com>
2026-05-13 11:40:23 +0700

    Add RootLayout component and metadata for app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Kling Motion',
+  description: 'Kling Motion Control'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
