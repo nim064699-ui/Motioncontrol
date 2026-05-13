8885136d66cb085b5b1ef54552abdebd358404ce
nim064699-ui <nim064699@gmail.com>
2026-05-13 12:03:17 +0700

    Implement file upload API endpoint

---
+++ app/api/upload/route.js
@@ -0,0 +1,30 @@
+export async function POST(req) {
+  try {
+    const data = await req.formData()
+
+    const file = data.get('file')
+
+    const formData = new FormData()
+
+    formData.append('reqtype', 'fileupload')
+    formData.append('fileToUpload', file)
+
+    const res = await fetch(
+      'https://catbox.moe/user/api.php',
+      {
+        method: 'POST',
+        body: formData
+      }
+    )
+
+    const url = await res.text()
+
+    return Response.json({
+      url
+    })
+  } catch (err) {
+    return Response.json({
+      error: err.message
+    })
+  }
+}
